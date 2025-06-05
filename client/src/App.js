58303f5a517108df14e619d03134f54422b0c74f
nehciyy <ycchiang001@mymail.sim.edu.sg>
2025-06-06 01:12:07 +0800

    feat: touch up on css and including all pages in app.js and index.js

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import HomePage from "./pages/HomePage";
+import ProfilePage from "./pages/ProfilePage";
+import SignUpPage from "./pages/SignupPage";
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
+    </Router>
+  );
+}
