c3a3419b336c0ff12471ce704cebf628160af897
nehciyy <ycchiang001@mymail.sim.edu.sg>
2025-06-10 23:25:09 +0800

    update Dockerfile

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ import authRoutes from "./routes/userRoutes.js";
 
 dotenv.config();
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
@@ -35,8 +36,9 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connected");
-    app.listen(5000, () => {
-      console.log("Server running on http://localhost:5000");
+    app.listen(PORT, () => {
+      console.log(`Server running on ${PORT}`);
+      console.log(`Server URL: http://localhost:${PORT} (if local)`);
     });
   })
   .catch((err) => console.error("MongoDB connection error:", err));
